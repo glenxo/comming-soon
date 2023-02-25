c34344580a9661dd8b5ebb61b090257ea17977f2
Glenn <glndxn80@gmail.com>
2023-02-25 00:41:01 -0500

    change layout

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
 window.addEventListener("resize", () => {
-    if (window.innerWidth >= 1200) {
-      menu.classList.remove("fa-times");
-      header.classList.remove("active");
-      document.body.classList.remove("active");
-    }next
-  });
+  if (window.innerWidth >= 1200) {
+    menu.classList.remove("fa-times");
+    header.classList.remove("active");
+    document.body.classList.remove("active");
+  }next
+});
+
+var initialTransforms = {};
+
+// Loop through each image
+for (var i = 1; i <= 6; i++) {
+  var img = document.getElementById("image" + i);
+  initialTransforms["image" + i] = img.style.transform;
+
+  img.onclick = function() {
+    this.style.transform = "scale(2)";
+
+  };
+
   
-  const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    navigation: {
-      prevEl: ".swiper-button-next",
-      nextEl: ".swiper-button-prev",
-      },
-    scrollbar: {
-      el: ".swiper-scrollbar",
-    },
-  });
-  
-  (function () {
-    const parallaxElements = document.querySelectorAll(".parallax");
-    function parallax() {
-      const scrollTop = window.pageYOffset;
-      for (let i = 0; i < parallaxElements.length; i++) {
-        const element = parallaxElements[i];
-        const speed = element.getAttribute("data-parallax-speed");
-        element.style.backgroundPositionY = scrollTop * speed + "px";
-      }
+  window.onscroll = function() {
+    for (var j = 1; j <= 6; j++) {
+      var img = document.getElementById("image" + j);
+      img.style.transform = initialTransforms["image" + j];
+      
     }
-  
-    window.addEventListener("scroll", parallax);
-  })();
+  };
+}
+
+(function () {
+  const parallaxElements = document.querySelectorAll(".parallax");
+  function parallax() {
+    const scrollTop = window.pageYOffset;
+    for (let i = 0; i < parallaxElements.length; i++) {
+      const element = parallaxElements[i];
+      const speed = element.getAttribute("data-parallax-speed");
+      element.style.backgroundPositionY = scrollTop * speed + "px";
+    }
+  }
+
+  window.addEventListener("scroll", parallax);
+})();
